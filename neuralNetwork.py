8e395537e9749855219f6e956d965268ce945810
Veerash Palanichamy <veerashp05@gmail.com>
2019-01-08 20:20:34 -0500

    updates

--- neuralNetwork.py
+++ neuralNetwork.py
@@ -2,7 +2,7 @@
 import numpy as np
 
 #imports the input
-import numInput
+#import numInput
 
 # network with layer size:
 # input layer = 900,  30*30 pixels
@@ -16,7 +16,7 @@ class neuralNetwork:
 
     def getOuput(self, a):
         for i in range(len(self.weights)):
-            a = sigmoid(np.dot(self.weights[i], a) + self.biases[i])
+            a = sigmoid(self.weights[i]*a + self.biases[i])
         return a
 
     def backprop(self, actual, expect):
@@ -24,6 +24,25 @@ class neuralNetwork:
 
     def cost(self, actual_value, expected_value):
         return (actual_value - expected_value);
+    
+    def backprop(self, a, b):
+        gradient_b = [np.zeros(np.shape(b)) for b in self.weights]
+        gradient_w = [np.zeros(np.shape(w)) for w in self.biases]
+        
+        activation = [a]
+        non_sigmoid = []
+        for i in range(len(self.weights)):
+            temp = np.dot(self.weights[i],a)+b
+            non_sigmoid.append(temp)
+            activation.append(sigmoid(temp))
+        
+        delta = (activation[1]-y)
+        gradient_w[1] = delta*primeSigmoid(non_sigmoid[1]).transpose()
+        gradient_b[1] = delta
+        
+        delta = self.weights[1].transpose*delta*primeSigmoid(non_sigmoid[0])
+        gradient_w[0] = delta
+        gradient_b[0] = delta*activation[0].transpose
 
 def sigmoid(x):
     return 1.0 / (1.0 + np.exp(-x))
